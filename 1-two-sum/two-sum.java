f4fcbc328e31684c32e4a642bd481e52e465e373
Aditya Sharma <259941928+adityasharma2408@users.noreply.github.com>
2026-08-25 12:29:35 +0530

    Time: 9 ms (46.42%) | Memory: 46.8 MB (85.33%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,48 @@
+// By using Sorting
+import java.util.*;
+
 class Solution {
+
+    class Pair {
+        int val;
+        int index;
+
+        Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
+
     public int[] twoSum(int[] nums, int target) {
-        int [] arr=new int[2];
-        for (int i = 0;i<nums.length-1;i++){
-            for (int j = i+1;j<nums.length;j++){
-                if (nums[i] + nums[j] == target){
-                    arr[0]=i;
-                    arr[1]=j;
-                }
+
+        ArrayList<Pair> a_p = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            a_p.add(new Pair(nums[i], i));
+        }
+
+        Collections.sort(a_p, (a, b) -> a.val - b.val);
+
+        int left = 0;
+        int right = a_p.size() - 1;
+
+        while (left < right) {
+
+            int sum = a_p.get(left).val + a_p.get(right).val;
+
+            if (sum == target) {
+                return new int[] {
+                    a_p.get(left).index,
+                    a_p.get(right).index
+                };
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return arr; 
+
+        return new int[] {};
     }
 }
