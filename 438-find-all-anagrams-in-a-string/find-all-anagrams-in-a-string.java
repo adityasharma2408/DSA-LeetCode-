57035fbca08de40b807006d9798713e340c78b7d
Aditya Sharma <259941928+adityasharma2408@users.noreply.github.com>
2026-09-07 15:03:06 +0530

    Time: 11 ms (66.53%) | Memory: 46.5 MB (95.87%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -4,18 +4,14 @@ class Solution {
         int [] p_table=new int[26];
         int[]  s_table=new int[26];
         int left=0;
-        int right=0;
         int k=p.length();
-
-
-
         for (char ch:p.toCharArray()){
             p_table[ch-'a']++;
         }
         for (int r=0;r<s.length();r++){
             char ch_l=s.charAt(r);
             s_table[ch_l-'a']++;
-            if(right-left+1==k){
+            if(r-left+1==k){
     if(Arrays.equals(p_table,s_table)){
         res.add(left);
     }
@@ -23,7 +19,6 @@ class Solution {
         s_table[ch_r-'a']--;
         left++;
         }
-        right++;
         }
         return res;
     }
